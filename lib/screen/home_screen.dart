7541c69b6358b73ee3b726338dbeb072e16c8d83
gha211th <agh.arrafif@gmail.com>
2026-04-16 08:47:03 +0700

    fix

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,7 +3,6 @@ import 'package:intl/intl.dart';
 import 'package:my_app/crud/db_model.dart';
 import 'package:my_app/font/reponsive_font.dart';
 import 'package:my_app/crud/db_helper.dart';
-import 'package:my_app/widgets/todo_form.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -19,6 +18,21 @@ class _HomeScreenState extends State<HomeScreen> {
   bool _showCompleted = false;
 
   DateTime? _selectedDate;
+
+  Future<void> _pickDate(BuildContext context) async {
+    DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2100),
+    );
+
+    if (picked != null) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
 
   void _refreshTask() {
     setState(() {});
@@ -183,7 +197,7 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             children: [
               _buildHeaderTitle(),
-              TodoForm(),
+              _buildTodoInput(),
               const SizedBox(height: 10),
               _buildDescInput(),
               const SizedBox(height: 10),
@@ -226,6 +240,61 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildTodoInput() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Add Task/List....",
+          style: TextStyle(
+            fontFamily: 'haas',
+            fontWeight: FontWeight.w700,
+            fontSize: 18,
+          ),
+        ),
+        Container(
+          child: TextField(
+            controller: _taskController,
+            readOnly: false,
+            decoration: InputDecoration(
+              hintText: 'Add Some Task/List...',
+              hintStyle: TextStyle(
+                fontFamily: 'haas',
+                color: Colors.black,
+                fontSize: 14,
+              ),
+              suffixIcon: IconButton(
+                onPressed: () => _pickDate(context),
+                icon: Icon(
+                  Icons.calendar_today_outlined,
+                  color: _selectedDate == null ? Colors.black : Colors.black,
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: const BorderSide(color: Colors.black, width: 2),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: const BorderSide(color: Colors.black, width: 2),
+              ),
+            ),
+          ),
+        ),
+        if (_selectedDate != null)
+          Padding(
+            padding: EdgeInsets.only(top: 8, left: 4),
+            child: Text(
+              "Target Date: ${DateFormat('yMMMd').format(_selectedDate!)}",
+            ),
+          ),
+      ],
     );
   }
 
--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -1,71 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/crud/db_helper.dart';
 
 class TodoItem extends StatelessWidget {
   const TodoItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 1, bottom: 1),
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(color: Colors.black, width: 2),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: TextStyle(
-                  fontFamily: 'haas',
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                  height: 1,
-                ),
-              ),
-              Text(
-                date,
-                style: TextStyle(
-                  fontFamily: 'haas',
-                  fontSize: 12,
-                  fontWeight: FontWeight.w500,
-                  height: 2,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              IconButton(
-                onPressed: () async {
-                  int newStatus = _showCompleted ? 0 : 1;
-                  await DbHelper.instance.updateStatus(id, newStatus);
-                  _refreshTask();
-                },
-                icon: Icon(
-                  _showCompleted ? Icons.check_circle : Icons.circle_outlined,
-                  size: 32,
-                  color: Colors.black,
-                ),
-              ),
-              IconButton(
-                icon: Icon(
-                  Icons.delete_outlined,
-                  size: 32,
-                  color: Colors.black,
-                ),
-                onPressed: () => _confirmDelete(id),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+    return const Placeholder();
   }
 }
